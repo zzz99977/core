b51692a2666a1afeaa9d56041a0d415e2ba6484c
Markus Mohrhard <markus.mohrhard@googlemail.com>
2013-12-27 21:01:55 +0100

    enable that code again
    Change-Id: I4314c7062195f0593c34b6a11ba2b3ae81811ff8

--- chart2/source/view/main/DummyXShape.cxx
+++ chart2/source/view/main/DummyXShape.cxx
@@ -220,10 +220,9 @@ void DummyXShape::setParent( const uno::Reference< uno::XInterface >& xParent )
 
 namespace {
 
-void setProperties( uno::Reference< beans::XPropertySet > , const tPropertyNameMap& ,
-        std::map<OUString, uno::Any>& )
+void setProperties( uno::Reference< beans::XPropertySet > xPropSet, const tPropertyNameMap& rPropertyNameMap,
+        std::map<OUString, uno::Any>& rTargetMap)
 {
-/*
     tNameSequence aNames;
     tAnySequence aValues;
     PropertyMapper::getMultiPropertyLists( aNames, aValues,
@@ -234,7 +233,6 @@ void setProperties( uno::Reference< beans::XPropertySet > , const tPropertyNameM
     {
         rTargetMap[aNames[i]] = aValues[i];
     }
-*/
 }
 
 }

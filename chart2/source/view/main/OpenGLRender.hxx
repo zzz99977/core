fc6543e04d22afc66fc1282cf6cc74822f4e8ae3
Markus Mohrhard <markus.mohrhard@collabora.co.uk>
2014-03-04 21:33:17 +0100

    remove mfAlpha again
    The concept with separate alpha will result in nasty alpha bugs.
    
    Change-Id: I9c18d2a850aeed2bf14f43d247029204013d3c93

--- chart2/source/view/main/OpenGLRender.hxx
+++ chart2/source/view/main/OpenGLRender.hxx
@@ -184,7 +184,6 @@ public:
 #endif
 
     void SetBackGroundColor(sal_uInt32 color1, sal_uInt32 color2, sal_uInt8 nAlpha);
-    void SetTransparency(sal_uInt32 transparency);
 private:
     GLint LoadShaders(const OUString& rVertexShaderName, const OUString& rFragmentShaderName);
     int CreateTextureObj(int width, int height);
@@ -287,7 +286,6 @@ private:
     GLuint m_DebugVertexID;
     GLuint m_DebugColorID;
 #endif
-    float m_fAlpha;
 };
 
 /* vim:set shiftwidth=4 softtabstop=4 expandtab: */

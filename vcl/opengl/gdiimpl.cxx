7e358eba7e908aace557268c4416a80c6823e00d
Jan Holesovsky <kendy@collabora.com>
2014-11-11 21:19:02 +0100

    windows opengl: Instantiate OpenGLSalBitmap even on Windows.
    Change-Id: I2229141cc12ad7bc9fccfd6e2c7fec44ac4f6284

--- vcl/opengl/gdiimpl.cxx
+++ vcl/opengl/gdiimpl.cxx
@@ -959,6 +959,9 @@ void OpenGLSalGraphicsImpl::copyBits( const SalTwoRect& rPosAry, SalGraphics* /*
 
 void OpenGLSalGraphicsImpl::drawBitmap( const SalTwoRect& rPosAry, const SalBitmap& rSalBitmap )
 {
+    // check that carefully only in the debug mode
+    assert(dynamic_cast<const OpenGLSalBitmap*>(&rSalBitmap));
+
     const OpenGLSalBitmap& rBitmap = static_cast<const OpenGLSalBitmap&>(rSalBitmap);
     GLuint nTexture = rBitmap.GetTexture( maContext );
     const Size aSize = rSalBitmap.GetSize();

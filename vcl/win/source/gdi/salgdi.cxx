151fdd5e4990389c2c2ceb3e69fb64d0e438db40
Jan Holesovsky <kendy@collabora.com>
2014-11-14 00:47:42 +0100

    vcl: Fix leak.
    Change-Id: I201e50060c44bba3c0eb20026be1daccd3b2e945

--- vcl/win/source/gdi/salgdi.cxx
+++ vcl/win/source/gdi/salgdi.cxx
@@ -615,7 +615,7 @@ WinSalGraphics::~WinSalGraphics()
     // delete cache data
     delete [] mpStdClipRgnData;
 
-    delete mpFontKernPairs;
+    delete [] mpFontKernPairs;
 }
 
 bool WinSalGraphics::isPrinter() const

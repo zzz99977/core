f186da3f21c3b6633123be95131f62d6b390491e
Louis-Francis Ratté-Boulianne <lfrb@collabora.com>
2014-12-04 22:25:56 -0500

    vcl: Re-use a framebuffer of the same size when possible
    Change-Id: Id9c7932976ce9d9282776c20d93d9cca4d290056

--- vcl/opengl/gdiimpl.cxx
+++ vcl/opengl/gdiimpl.cxx
@@ -119,6 +119,8 @@ void OpenGLSalGraphicsImpl::Init()
         maOffscreenTex.GetWidth()  != GetWidth() ||
         maOffscreenTex.GetHeight() != GetHeight() )
     {
+        if( mpContext ) // valid context
+            mpContext->ReleaseFramebuffer( maOffscreenTex );
         maOffscreenTex = OpenGLTexture();
     }
 }
@@ -160,15 +162,18 @@ void OpenGLSalGraphicsImpl::PostDraw()
         mpProgram = NULL;
     }
 
-    mpContext->ReleaseFramebuffer( mpFramebuffer );
-    mpFramebuffer = NULL;
-
     CHECK_GL_ERROR();
 }
 
 void OpenGLSalGraphicsImpl::freeResources()
 {
     // TODO Delete shaders, programs and textures if not shared
+    if( mbOffscreen && mpContext && mpContext->isInitialized() )
+    {
+        mpContext->makeCurrent();
+        mpContext->ReleaseFramebuffer( maOffscreenTex );
+    }
+    ReleaseContext();
 }
 
 void OpenGLSalGraphicsImpl::ImplSetClipBit( const vcl::Region& rClip, GLuint nMask )
@@ -1442,6 +1447,7 @@ void OpenGLSalGraphicsImpl::endPaint()
     if( mpContext->mnPainting == 0 && !mbOffscreen )
     {
         mpContext->makeCurrent();
+        mpContext->AcquireDefaultFramebuffer();
         glFlush();
     }
 }

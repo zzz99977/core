987a7c933836282cb4c0cf46ec55d98a61461594
matteocam <matteo.campanelli@gmail.com>
2015-06-04 10:42:47 -0400

    Calling ReformatText after setting para object in src box
    Change-Id: I1c27ec0e91a248d894af929dde11a311089de259

--- svx/source/svdraw/svdotextdecomposition.cxx
+++ svx/source/svdraw/svdotextdecomposition.cxx
@@ -730,7 +730,7 @@ void SdrTextObj::impDecomposeContourTextPrimitive(
 
 void SdrTextObj::impLeaveOnlyNonOverflowingText() const
 {
-    // Cut non overflowing text // FIXME: Move this in separate function
+    // Cut non overflowing text
     NonOverflowingText *pNonOverflowingTxt =
         pEdtOutl->GetNonOverflowingText();
     SdrOutliner &rOutliner = ImpGetDrawOutliner();
@@ -756,6 +756,7 @@ void SdrTextObj::impLeaveOnlyNonOverflowingText() const
 
     OutlinerParaObject *pNewText = rOutliner.CreateParaObject();
     const_cast<SdrTextObj*>(this)->NbcSetOutlinerParaObject(pNewText);
+    const_cast<SdrTextObj*>(this)->ReformatText();
 }
 
 

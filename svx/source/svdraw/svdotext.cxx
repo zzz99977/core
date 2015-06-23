df9fccced03eee9092d86d579f46d93583a82433
matteocam <matteo.campanelli@gmail.com>
2015-06-23 19:42:20 -0400

    Minor changes
    Change-Id: I36d52878596750bc275a4ddd7dc79a9e2191becb

--- svx/source/svdraw/svdotext.cxx
+++ svx/source/svdraw/svdotext.cxx
@@ -2027,20 +2027,13 @@ void SdrTextObj::onUnderflowStatusEvent( )
     if (bIsOverflowFromUnderflow) {
         // prevents infinite loops when setting text for editing outliner
         GetTextChain()->SetNilChainingEvent(const_cast<SdrTextObj*>(this), true);
+
         impLeaveOnlyNonOverflowingText(&aDrawOutliner);
         impMoveChainedTextToNextLink(&aDrawOutliner, pNextLink);
     } else {
+        // No overflow: set the whole thing
         const_cast<SdrTextObj*>(this)->SetOutlinerParaObject(pNewText);
     }
-
-    /*
-    if (pEdtOutl != NULL)
-        pEdtOutl->SetText(*pNewText);
-    */
-
-    // Don't need this if handling everything here
-    //const_cast<SdrTextObj*>(this)->SetOutlinerParaObject(pNewText);
-
 }
 
 
@@ -2052,7 +2045,7 @@ void SdrTextObj::onChainingEvent()
     if (!pEdtOutl)
         return;
 
-
+    // This is true during an underflow-caused overflow (with pEdtOutl->SetText())
     if (GetTextChain()->GetNilChainingEvent(this)) {
         GetTextChain()->SetNilChainingEvent(this, false);
         return;
@@ -2072,7 +2065,6 @@ void SdrTextObj::onChainingEvent()
     } else {
         onUnderflowStatusEvent();
     }
-    return;
 }
 
 
